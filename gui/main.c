f267d9a867b6d1dda840b12701976cbf53b59ee2
Fabio Leuthold <fabio.leuthold@hotmail.ch>
2025-11-23 00:43:04 +0100

    der folgende befehl von chatgpt war der der alles zerstörte:
    DrawFrameControl
    
    bzw
    
    DrawFrameControl(hdc, &rc, DFC_BUTTON, DFCS_BUTTONPUSH | ((state & CDIS_SELECTED) ? DFCS_PUSHED : 0));

--- gui/main.c
+++ gui/main.c
@@ -21,10 +21,50 @@ LRESULT CALLBACK DlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         LPNMCUSTOMDRAW item = (LPNMCUSTOMDRAW)lParam;
         if (item->hdr.idFrom == IDC_BUTTON2 && item->hdr.code == NM_CUSTOMDRAW)
         {
-                
+
+            HDC hdc = item->hdc;
+            RECT rc = item->rc;
+            UINT state = item->uItemState;
+            COLORREF normal = RGB(0, 255, 255); // purple 
+            COLORREF hover = RGB(0, 255, 160); // lighter 
+            COLORREF pressed = RGB(96, 0, 128); // darker 
+            COLORREF bg; 
+            if (state & CDIS_SELECTED) bg = pressed; 
+            else if (state & CDIS_HOT) bg = hover; 
+            else bg = normal; 
+
+            HBRUSH br = CreateSolidBrush(bg);
+
+
+
+            //HPEN pen = CreatePen(PS_INSIDEFRAME, 0, RGB(0, 0, 0));
+
+
+            //HGDIOBJ old_pen = SelectObject(item->hdc, pen);
+            //HGDIOBJ old_brush = SelectObject(item->hdc, br);
+            //RoundRect(item->hdc, item->rc.left, item->rc.top, item->rc.right, item->rc.bottom, 10, 10);
+            FillRect(hdc, &rc, br);
+            DeleteObject(br);
+            //SelectObject(item->hdc, old_pen);
+            //SelectObject(item->hdc, old_brush);
+            //DeleteObject(pen);
+            //DrawFrameControl(hdc, &rc, DFC_BUTTON, DFCS_BUTTONPUSH | ((state & CDIS_SELECTED) ? DFCS_PUSHED : 0)); // Draw text centered 
+            //wchar_t text[128]; GetWindowText(item->hdr.hwndFrom, text, 128); SetBkMode(hdc, TRANSPARENT);
+            //SetTextColor(hdc, RGB(255, 255, 255)); // white text 
+            //DrawText(hdc, text, -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+            /*
+            FillRect(hdc, &rc, br); 
+            DeleteObject(br); // Draw themed button frame 
+            DrawFrameControl(hdc, &rc, DFC_BUTTON, DFCS_BUTTONPUSH | ((state & CDIS_SELECTED) ? DFCS_PUSHED : 0)); // Draw text centered 
+            wchar_t text[128]; GetWindowText(item->hdr.hwndFrom, text, 128); SetBkMode(hdc, TRANSPARENT);
+            SetTextColor(hdc, RGB(255, 255, 255)); // white text 
+            DrawText(hdc, text, -1, &rc, DT_CENTER | DT_VCENTER | DT_SINGLELINE); */
+            return CDRF_DODEFAULT; // do NOT let Windows overpaint
+
+
+                /*
                 COLORREF bg = RGB(0,255,255);
                 if (item->uItemState & CDIS_SELECTED)
-                
                 {
                     bg = RGB(96, 0, 128);
                     
@@ -46,7 +86,7 @@ LRESULT CALLBACK DlgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
 
                 return CDRF_DODEFAULT;
-
+                */
                 
         }
         break;
